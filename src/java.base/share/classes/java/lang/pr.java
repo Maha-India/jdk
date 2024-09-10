4778969e69dd0a5249fd2f3e84f40a9265e951a3
Maha-India <131330837+Maha-India@users.noreply.github.com>
2024-09-10 19:44:05 +0530

    Create pr.java

---
+++ src/java.base/share/classes/java/lang/pr.java
@@ -0,0 +1,101 @@
+package java.lang;
+
+public class pr {
+    // Redirects to System.out's methods
+
+    // Equivalent to System.out.print(Object obj)
+    public static void print(Object obj) {
+        System.out.print(obj);
+    }
+
+    // Equivalent to System.out.print(boolean b)
+    public static void print(boolean b) {
+        System.out.print(b);
+    }
+
+    // Equivalent to System.out.print(char c)
+    public static void print(char c) {
+        System.out.print(c);
+    }
+
+    // Equivalent to System.out.print(char[] cbuf)
+    public static void print(char[] cbuf) {
+        System.out.print(cbuf);
+    }
+
+    // Equivalent to System.out.print(double d)
+    public static void print(double d) {
+        System.out.print(d);
+    }
+
+    // Equivalent to System.out.print(float f)
+    public static void print(float f) {
+        System.out.print(f);
+    }
+
+    // Equivalent to System.out.print(int i)
+    public static void print(int i) {
+        System.out.print(i);
+    }
+
+    // Equivalent to System.out.print(long l)
+    public static void print(long l) {
+        System.out.print(l);
+    }
+
+    // Equivalent to System.out.println()
+    public static void println() {
+        System.out.println();
+    }
+
+    // Equivalent to System.out.println(Object obj)
+    public static void println(Object obj) {
+        System.out.println(obj);
+    }
+
+    // Equivalent to System.out.println(boolean b)
+    public static void println(boolean b) {
+        System.out.println(b);
+    }
+
+    // Equivalent to System.out.println(char c)
+    public static void println(char c) {
+        System.out.println(c);
+    }
+
+    // Equivalent to System.out.println(char[] cbuf)
+    public static void println(char[] cbuf) {
+        System.out.println(cbuf);
+    }
+
+    // Equivalent to System.out.println(double d)
+    public static void println(double d) {
+        System.out.println(d);
+    }
+
+    // Equivalent to System.out.println(float f)
+    public static void println(float f) {
+        System.out.println(f);
+    }
+
+    // Equivalent to System.out.println(int i)
+    public static void println(int i) {
+        System.out.println(i);
+    }
+
+    // Equivalent to System.out.println(long l)
+    public static void println(long l) {
+        System.out.println(l);
+    }
+
+    // Equivalent to System.out.printf(String format, Object... args)
+    public static void printf(String format, Object... args) {
+        System.out.printf(format, args);
+    }
+
+    // Equivalent to System.out.printf(Locale l, String format, Object... args)
+    public static void printf(java.util.Locale l, String format, Object... args) {
+        System.out.printf(l, format, args);
+    }
+  }
+                              
